753e5463fae1e72a0065552c2d5016be92e4b3ce
Xenia Meraki <xeniameraki@protonmail.com>
2025-12-03 09:00:57 -0700

    Update ci_dummy_hook_postrootfs.sh

--- .github/workflows/ci_dummy_hook_postrootfs.sh
+++ .github/workflows/ci_dummy_hook_postrootfs.sh
@@ -35,36 +35,6 @@ hidden_spokes =
 EOF
 fi
 
-# Get Artwork
-git clone https://github.com/ublue-os/packages.git /root/packages
-case "${PRETTY_NAME,,}" in
-"aurora"*)
-    mkdir -p /usr/share/anaconda/pixmaps/silverblue
-    cp -r /root/packages/aurora/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
-    cp -r /root/packages/aurora/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/silverblue/
-    ;;
-"bazzite"*)
-    mkdir -p /usr/share/anaconda/pixmaps/silverblue
-    cp -r /root/packages/bazzite/fedora-logos/* /usr/share/anaconda/pixmaps/
-    ;;
-"bluefin"*)
-    mkdir -p /usr/share/anaconda/pixmaps/silverblue
-    cp -r /root/packages/bluefin/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
-    cp -r /root/packages/bluefin/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/silverblue/
-    ;;
-esac
-rm -rf /root/packages
-
-# Variables
-imageref="$(jq -r '."image-ref"' </usr/share/ublue-os/image-info.json)"
-imageref="${imageref##*://}"
-imagetag="$(jq -r 'if ."image-branch" then ."image-branch" else ."image-tag" end' </usr/share/ublue-os/image-info.json)"
-sbkey='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der'
-
-# Secureboot Key Fetch
-mkdir -p /run/install/repo
-curl -Lo /run/install/repo/sb_pubkey.der "$sbkey"
-
 # Default Kickstart
 cat <<EOF >>/usr/share/anaconda/interactive-defaults.ks
 ostreecontainer --url=$imageref:$imagetag --transport=containers-storage --no-signature-verification
@@ -72,42 +42,6 @@ ostreecontainer --url=$imageref:$imagetag --transport=containers-storage --no-si
 %include /usr/share/anaconda/post-scripts/disable-fedora-flatpak.ks
 %include /usr/share/anaconda/post-scripts/install-flatpaks.ks
 %include /usr/share/anaconda/post-scripts/secureboot-enroll-key.ks
-EOF
-
-# Signed Images
-cat <<EOF >>/usr/share/anaconda/post-scripts/install-configure-upgrade.ks
-%post --erroronfail
-bootc switch --mutate-in-place --enforce-container-sigpolicy --transport registry $imageref:$imagetag
-%end
-EOF
-
-# Enroll Secureboot Key
-cat <<EOF >>/usr/share/anaconda/post-scripts/secureboot-enroll-key.ks
-%post --erroronfail --nochroot
-set -oue pipefail
-
-readonly ENROLLMENT_PASSWORD="universalblue"
-readonly SECUREBOOT_KEY="/run/install/repo/sb_pubkey.der"
-
-if [[ ! -d "/sys/firmware/efi" ]]; then
-	echo "EFI mode not detected. Skipping key enrollment."
-	exit 0
-fi
-
-if [[ ! -f "\$SECUREBOOT_KEY" ]]; then
-	echo "Secure boot key not provided: \$SECUREBOOT_KEY"
-	exit 0
-fi
-
-SYS_ID="\$(cat /sys/devices/virtual/dmi/id/product_name)"
-if [[ ":Jupiter:Galileo:" =~ ":\$SYS_ID:" ]]; then
-	echo "Steam Deck hardware detected. Skipping key enrollment."
-	exit 0
-fi
-
-mokutil --timeout -1 || :
-echo -e "\$ENROLLMENT_PASSWORD\n\$ENROLLMENT_PASSWORD" | mokutil --import "\$SECUREBOOT_KEY" || :
-%end
 EOF
 
 # Install Flatpaks
